a62bd6f910658e428866914a3d617f7613de3f86
Wes DeKoninck <wes@saal.net>
2014-05-09 15:49:41 -0400

    Update WP admin footer credits

--- library/admin.php
+++ library/admin.php
@@ -109,7 +109,7 @@ you like.
 
 // Custom Backend Footer
 function bones_custom_admin_footer() {
-	_e( '<span id="footer-thankyou">Developed by <a href="http://yoursite.com" target="_blank">Your Site Name</a></span>. Built using <a href="http://themble.com/bones" target="_blank">Bones</a>.', 'bonestheme' );
+	_e( '<span id="footer-thankyou">Developed by <a href="http://saaladvertising.com" target="_blank">SAAL Advertising</a></span>.', 'bonestheme' );
 }
 
 // adding it to the admin area
